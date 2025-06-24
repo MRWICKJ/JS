30c29baffd2a0bf6adc91fbdcb6e4817bad22e15
MRWICKJ <shubhenduhalder4@gmail.com>
2025-06-25 01:29:48 +0530

    Add comparison and string manipulation examples in comparision.js and string.js

---
+++ Day1/comparision.js
@@ -0,0 +1,14 @@
+console.log(2 == 2)
+console.log(2 < 2)
+console.log(2 > 2)
+console.log(2 >= 2)
+console.log(2 <= 2)
+console.log(2 === 2)
+console.log(2 != 2)
+console.log(2 !== 2)
+
+
+
+console.log(undefined === null)
+console.log(Number("5a"))
+console.log(NaN == NaN);
---
+++ Day1/string.js
@@ -0,0 +1,8 @@
+let a = "Jaya"
+
+console.log(a.length)
+console.log(a.substring(0, 2))
+console.log(a.indexOf("a"))
+
+console.log("1"+1)
+console.log(1/"1")
