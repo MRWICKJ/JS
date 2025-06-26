dc970d3307a409b5f1af3b1b6fc2d34874538068
MRWICKJ <shubhenduhalder4@gmail.com>
2025-06-27 00:43:43 +0530

    Add initial implementation of stack and heap examples in stack_heap.js

---
+++ Day1/stack_heap.js
@@ -0,0 +1,17 @@
+let first = "first"
+let last = first
+last = "last"
+console.log(first)
+console.log(last)
+
+let obj1 = {
+    name:"aman",
+    age:18
+}
+
+let obj2 = obj1
+
+obj2.age = 20;
+
+console.log(obj1);
+console.log(obj2)
