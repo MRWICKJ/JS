8f280f6298e655ba2263c9e6c4d502877a04aafa
MRWICKJ <shubhenduhalder4@gmail.com>
2025-07-16 01:08:07 +0530

    Add initial implementations of destructuring, prime number check, and sum of array examples

---
+++ Day1/destructure.js
@@ -0,0 +1,3 @@
+let person = { name: "Alice", age: 22 };
+let { name, age } = person;
+console.log(name, age);
---
+++ Day1/sumOfArray.js
@@ -0,0 +1,8 @@
+let nums = [10, 20, 30];
+let total = 0;
+
+for (let i = 0; i < nums.length; i++) {
+  total = total + nums[i];
+}
+
+console.log("Total:", total);
